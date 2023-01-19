2a4a21a4123d9cf78f318f3470ba19568896227f
fanhuan <hfan22@wisc.edu>
2023-01-19 16:11:40 +0800

    2022

--- 2022/AAF-GWAS_figures_2022.R
+++ 2022/AAF-GWAS_figures_2022.R
@@ -3,8 +3,10 @@ library(ggplot2)
 library(dplyr)
 library(gridExtra)
 library(hash)
+library(parallel)
 # Figure 1: 100 rpoBsimulation for all 11 scoring methods.
 output.scores <- read_csv("rpoBsimulation/sp100d01/S450_summary/rpoBsimulation_output_scores_1Dec21.csv")
+output.scores <- read_csv("rpoBsimulation/sp100d01/S450_summary/rpoBsimulation_output_scores_thr10_1Dec21.csv")
 # Rename the models
 old_model <- c('LS','GLS','EGLS','LogF','PhyLog','PLogML_LR_a',
                'PLog','PLogML','PLogML_LR','PLogML_LR_p','phyloglmA')
@@ -101,9 +103,143 @@ irep113 %>% filter(Model == 'LogF') %>% arrange(Rank_450)
 # Because not all patterns gets the same type! The ones around 0 can sometimes be positive and sometimes negative!
 # I could look into why but the ones around 0 is trivial. 
 # What I worry more is why in the mean plot the second rank dipped 
-# 1. inbalanced data resulted in very bad Rank for Rank_450=1 (fixed by increasing threshold to 4).
+# 1. inbalanced kmer pattern (too many 1s or too many 0s) resulted in very bad Rank for Rank_450=1 (fixed by increasing threshold to 4).
+# But we cannot just remove them. They are true positive even if the complexity is low.
+# A more reasonable way is to remove simulations with inbalanced trait data.
+# Currently, simulations with at least 3 serines and 3 non-serines are saved. 
+# Let's see whether increase the threshold would help.
+s <- phylosim_sp100d01_49_trait[phylosim_sp100d01_49_trait$serine == 'TRUE',]
+dim(s)[1]
+c <- rep(0,115)
+for (i in 0:114){
+  trait <- read_csv(paste0("rpoBsimulation/sp100d01/trait/phylosim_sp100d01_", i, 
+                           "_trait.csv"))
+  s <- trait[trait$serine == 'FALSE',]
+  c[i+1] <- dim(s)[1]
+}
+hist(c)
+# The lowest mutations are 4 (too litter info?) and the highest is 93 (also pretty
+# in balanced). But is there a correlation of the imbalance of the trait data 
+# versus the imbalance in the true positive k-mer patterns?
+
+da <- read_csv("rpoBsimulation/sp100d01/S450_summary/rpoBsimulation_output_scores_1Dec21.csv")
+da <- read_csv("rpoBsimulation/sp100d01/S450_summary/rpoBsimulation_output_scores_thr10_1Dec21.csv")
+da %>% group_by(Model) %>% tally()
+thr = 10
+da_LS <- da %>% filter(Model == 'LS') %>% 
+  mutate(Group = ifelse(sumy < thr, 'imbalanced', 
+                        ifelse(sumy > 100-thr, 'imbalanced', 'balanced'))) %>% 
+  group_by(irep, Group) %>% tally() %>% group_by(irep) %>% mutate(per_kmer = n/sum(n))
+irep <- seq(0,114)
+ids <- cbind(c, irep)
+da <- merge(da, ids, by = 'irep')
+da_LS <- merge(da_LS, ids, by = 'irep')
+da_LS_im <- da_LS %>% filter(Group == 'imbalanced')
+da_zero <- da %>% filter(Rank == 0) %>% group_by(Model) %>% tally()
+da_one <- da %>% filter(Rank == 1) %>% group_by(Model) %>% tally() 
+# 536 true positives in our 115 simulations, 313 was able to rank it 1st among all k-mers.
+da_one_irep <- da %>% filter(Rank == 1) %>% group_by(Model, irep, Type) %>% tally()
+length(unique(da_one_irep$irep))
+# So among 115 simulations, there are 101 ended up having the first ranked k-mer being true positives.
+da_one_type <- da_one_irep %>% group_by(Model,Type) %>% tally()
+# If we don't distinguish positive/negative types, among those 101 ireps, PLogML_LR_a performed the best(100 ireps) followed by GLS, 
+# PLogML_LRand PLogML_LR_p (ireps 99) 
+da_one_type <- da_one_irep %>% group_by(Model,Type) %>% tally()
+# But if we do, GLS performs the best in terms of the percentage of ireps with the highest positive scores being a true positive kmer pattern.
+# Interestingly, the negative patterns peformed way better (6 methods had 95 ireps among 101)
+# So now two things to check:
+# 1. What is the biological meaning of negative k-mer patterns. Are their counterparts (1101 vs. 0010)
+# instantly scores the same with a different sign?
+
+# How do we utilize the negative info?
+
+# 2. If we combine different methods, would it increase the number of ireps with the highest positve score being true (now highest is GLS 81)?
+
+plot(da_LS_im$c, da_LS_im$per) %>% group_by(irModel) %>% tally
+plot(da_LS)
+# how to summarize imbalance?
+hist(da$sumy)
+# It seems like the true positive k-mer patterns tend to be distributed at the two ends of sumy.
+da %>% group_by(Model) %>% tally() %>% arrange(n)
+# Every truo positive k-mer pattern was scored using all methods. Let's only use rows under LS as a way to dereplicate.
+
 d_n2 <- d %>% filter(Rank_450 < 3)
 tmp <- d_n2 %>% filter(model_f == "Phylog_EstA_LR",Type == 'Negative')
 tmp <- d %>% filter(irep == '20', Type == 'Negative')
 tmp <- d %>% filter(irep == '20', Model == 'phyloglmA')
+
+# The result for imbalanced k-mer patterns are less trustful. Therefore we will 
+# filter out all imbalanced k-mers and re-rank. Where is the original data?
+prefix_base <- 'phylosim_sp100d01_'
+repn <- 115
+date <- '1Dec21'
+thr = 10 
+model_list = c('LS','LogF','GLS','EGLS','PhyLog','PLogML_LR_a','PLog','phyloglmA','PLogML_all')
+if ('PLogML_all' %in% model_list){
+  index <- match('PLogML_all',model_list)
+  model_name_list <- c(model_list[1:index-1],"PLogML", "PLogML_LR", "PLogML_LR_p", 
+                       model_list[index+1:length(model_list)])[1:(length(model_list)+2)]
+}else{
+  model_name_list <- model_list            
+}
+data_dir <- "rpoBsimulation/sp100d01/"
+prefix_base <- 'phylosim_sp100d01_'
+output.scores <- NULL #saving all S450 scores
+date <- '1Dec21'
+for (irep in 0:114) { # count from 0
+  print(irep)
+  prefix <- paste0(prefix_base,irep)
+  score <- as.data.frame(read_csv(paste0("rpoBsimulation/sp100d01/score/phylosim_sp100d01_", 
+                           irep,"_scores_1Dec21.csv")))
+  score.ba <- score %>% filter(sumy > thr, sumy < 100-thr)
+  score.ba <- score.ba[ , !grepl( "rank" , names( score.ba ) ) ]
+  reps_m <- seq(length(model_name_list))
+  # x+3 because now there is an extra column of freq.
+  r_m <- mclapply(reps_m, function(x){rank_fh(as.matrix(score.ba[,x+3]))},
+                  mc.cores = 4)
+  output_rank <- as.data.frame(do.call(cbind, r_m))
+  names(output_rank)<-c(sapply('rank',paste0,model_name_list,USE.NAMES = F))
+  output <- cbind(score.ba,output_rank)
+  S450_kmer <- read.csv(paste0(data_dir, 'S450_kmer_pattern/', prefix, '_S450.kmer'),colClasses = c("character", "character"))
+  S450_score <- merge(S450_kmer, output, by = "pattern")
+  if (dim(S450_score)[1] > 0){
+    # Summarize into one excel sheet like rpoBsimulation_output_scores_20Aug2021.csv
+    output_450_plot <- S450_score %>% select(-kmer)
+    output_450_plot <- unique(output_450_plot) 
+    # rearrange the data set so score and rank for one method is together
+    order_c <- rep(0,length(model_name_list) * 2)
+    for (i in 1:length(model_name_list)){
+      # j is the position in order_c
+      j = i*2
+      # position of the score
+      order_c[j-1] = 3 + i
+      # position of the rank
+      order_c[j] = 3 + length(model_name_list) + i
+    }
+    output_450_plot <- output_450_plot[,c(1:3,order_c)]
+    # reshape into long form
+    output_450_plot_long <- output_450_plot %>% reshape(varying=4:ncol(output_450_plot),
+                                                        timevar="Model",
+                                                        times = model_name_list,
+                                                        v.names = c("Rank","Score"),
+                                                        direction = "long",
+                                                        new.row.names=1:10000) %>%
+      # for some reason the rank and score is swapped
+      mutate(Score1=Rank, Rank=Score, Score=Score1,Model = as.factor(Model),Rank_450=NA) %>%
+      dplyr::select(-c(Score1,id)) %>% mutate(Type = ifelse(is.na(Score), 'notappilicable',
+                                                            ifelse(Score > 0, 'Positive',
+                                                                   ifelse(Score == 0, 0, 'Negative'))))
+    output_450_rank <- output_450_plot_long[0,]
+    for (md in levels(output_450_plot_long$Model)) {
+      subset <- output_450_plot_long %>% filter(Model == md)
+      subset$Rank_450 <- rank_fh(subset$Score)
+      output_450_rank <- rbind(output_450_rank,subset)
+    }
+    output.scores <- rbind(output.scores, cbind(array(irep, dim=dim(output_450_rank)[1]),output_450_rank[,2:ncol(output_450_rank)]))
+  }else{
+    print(paste('all S450 patterns are imbalanced from irep', irep))
+  }
+}
+names(output.scores)[1] <- 'irep'
+write.csv(file = paste0(data_dir, "S450_summary/rpoBsimulation_output_scores_thr", thr, '_',date, ".csv"), output.scores, row.names = F)
 
---
+++ rpoBSimulations_8Nov21.R
@@ -0,0 +1,184 @@
+#One simulation
+#dependencies
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("Biostrings")
+
+library(phylosim)
+# phylosim no longer supported in CRAN. To get the latest version from https://cran.r-project.org/src/contrib/Archive/phylosim/
+library(ape)
+library(Biostrings)
+library(phangorn)
+library(phylobase)
+library(adephylo)
+
+#Phylosim settings
+#Enable the "fast and careless mode"
+PSIM_FAST <- TRUE
+
+#codon freqs for flanking regions are calculated based on 11 rpoB genes in Mycobacterium genus, stored in rpoB_alignment.fa 
+codon.freqs.flanking <- c(0.0017830839599969, 0.0276765640747345, 7.75253895650826e-05, 0.0100007752538957, 
+                          0.00217071090782231, 0.017830839599969, 0.00186060934956198, 0.0233351422590899, 0.00139545701217149, 
+                          0.0186836188851849, 0.00139545701217149, 0.00565935343825103, 0.00767501356694317, 0.0031010155826033, 
+                          0.0128692146678037, 0.000930304674780991, 0.0560508566555547, 0.00248081246608264, 0.013101790836499, 
+                          0.00240328707651756, 0.0368245600434142, 0.0013179316226064, 0.0174432126521436, 0.00240328707651756, 
+                          0.0269788355686487, 0.0106209783704163, 0.0382975424451508, 0.00248081246608264, 0.0179083649895341, 
+                          0.00449647259477479, 0.0512442825025196, 0.000387626947825413, 0.0259710055043027, 0.0031010155826033, 
+                          0.0369796108225444, 0.00155050779130165, 0.0133343670051942, 0.00186060934956198, 0.0323280874486394, 
+                          0.00317854097216838, 0.0396929994573223, 0.0035661679199938, 0.00922552135824482, 0, 0.000697728506085743, 
+                          0.00627955655477169, 0.0421738119234049, 0.000930304674780991, 0.0434142181564462, 0.00465152337390495, 
+                          0.0372121869912396, 0.00348864253042872, 0.0255058531669122, 0.00814016590433367, 0.062020311652066, 
+                          0.0146522986278006, 0.0747344755407396, 0.0240328707651756, 0.0464377083494845, 0.0035661679199938, 
+                          0.0103884022017211)
+
+p.flanking <- GY94(codon.freqs = codon.freqs.flanking)
+p.flanking$kappa = 2 #kappa is the transition/transversion rate
+
+##set up hotspot
+#6 codons for serine
+serine.codons <- c("TCT", "TCC", "TCA", "TCG", "AGT", "AGC")
+
+#construct the root sequence (M.canettii)
+string="TTGGCAGATTCCCGCCAGAGCAAAACAGCCGCTAGTCCTAGTCCGAGTCGCCCGCAAAGTTCCTCGAATAACTCCGTACCCGGAGCGCCAAACCGGGTCTCCTTCGCTAAGCTGCGCGAACCACTTGAGGTTCCGGGACTCCTTGACGTCCAGACCGATTCGTTCGAGTGGCTGATCGGTTCGCCGCGCTGGCGCGAATCCGCCGCCGAGCGGGGTGATGTCAACCCAGTGGGTGGCCTGGAAGAGGTGCTCTACGAGCTGTCTCCGATCGAGGACTTCTCCGGGTCGATGTCGTTGTCGTTCTCTGACCCTCGTTTCGACGATGTCAAGGCACCCGTCGACGAGTGCAAAGACAAGGACATGACGTACGCGGCTCCACTGTTCGTCACCGCCGAGTTCATCAACAACAACACCGGTGAGATCAAGAGTCAGACGGTGTTCATGGGTGACTTCCCGATGATGACCGAGAAGGGCACGTTCATCATCAACGGGACCGAGCGTGTGGTGGTCAGCCAGCTGGTGCGGTCGCCCGGGGTGTACTTCGACGAGACCATTGACAAGTCCACCGACAAGACGCTGCACAGCGTCAAGGTGATCCCGAGCCGCGGCGCGTGGCTCGAGTTTGACGTCGACAAGCGCGACACCGTCGGCGTGCGCATCGACCGCAAACGCCGGCAACCGGTCACCGTGCTGCTCAAGGCGCTGGGCTGGACCAGCGAGCAGATTGTCGAGCGGTTCGGGTTCTCCGAGATCATGCGATCGACGCTGGAGAAGGACAACACCGTCGGCACCGACGAGGCGCTGTTGGACATCTACCGCAAGCTGCGTCCGGGCGAGCCCCCGACCAAAGAGTCAGCGCAGACGCTGTTGGAAAACTTGTTCTTCAAGGAGAAGCGCTACGACCTGGCCCGCGTCGGTCGCTATAAGGTCAACAAGAAGCTCGGGCTGCATGTCGGCGAGCCCATCACGTCGTCGACGCTGACCGAAGAAGACGTCGTGGCCACCATCGAATATCTGGTCCGCTTGCACGAGGGTCAGACCACGATGACCGTTCCGGGCGGCGTCGAGGTGCCGGTGGAAACCGACGACATCGACCACTTCGGCAACCGCCGCCTGCGTACGGTCGGCGAGCTGATCCAAAACCAGATCCGGGTCGGCATGTCGCGGATGGAGCGGGTGGTCCGGGAGCGGATGACCACCCAGGACGTGGAGGCGATCACACCGCAGACGTTGATCAACATCCGGCCGGTGGTCGCCGCGATCAAGGAGTTCTTCGGCACCAGCCAGCTGAGCCAATTCATGGACCAGAACAACCCGCTGTCGGGGTTGACCCACAAGCGCCGACTGTCGGCGCTGGGGCCCGGCGGTCTGTCACGTGAGCGTGCCGGGCTGGAGGTCCGCGACGTGCACCCGTCGCACTACGGCCGGATGTGCCCGATCGAAACCCCTGAGGGGCCCAACATCGGTCTGATCGGCTCGCTGTCGGTGTACGCGCGGGTCAACCCGTTCGGGTTCATCGAAACGCCGTACCGCAAGGTGGTCGACGGCGTGGTTAGCGACGAGATCGTGTACCTGACCGCCGACGAGGAGGACCGCCACGTGGTGGCACAGGCCAATTCGCCGATCGATGCGGACGGTCGCTTCGTCGAGCCGCGCGTGCTGGTCCGCCGCAAGGCGGGCGAGGTGGAGTACGTGCCCTCGTCTGAGGTGGACTACATGGACGTCTCGCCCCGCCAGATGGTGTCGGTGGCCACCGCGATGATTCCCTTCCTGGAGCACGACGACGCCAACCGTGCCCTCATGGGGGCAAACATGCAGCGCCAGGCGGTGCCGCTGGTCCGTAGCGAGGCCCCGCTGGTGGGCACCGGGATGGAGCTGCGCGCGGCGATCGACGCCGGCGACGTCGTCGTCGCCGAAGAAAGCGGCGTCATCGAGGAGGTGTCGGCCGACTACATCACTGTGATGCACGACAACGGCACCCGGCGTACCTACCGGATGCGCAAGTTTGCCCGGTCCAACCACGGCACTTGCGCCAACCAGTGCCCCATCGTGGACGCGGGCGACCGAGTCGAGGCCGGTCAGGTGATCGCCGACGGTCCCTGTACTGACGACGGCGAGATGGCGCTGGGCAAGAACCTGCTGGTGGCCATCATGCCGTGGGAGGGCCACAACTACGAGGACGCGATCATCCTGTCCAACCGCCTGGTCGAAGAGGACGTGCTCACCTCGATCCACATCGAGGAGCATGAGATCGATGCTCGCGACACCAAGCTGGGTGCGGAGGAGATCACCCGCGACATCCCGAACATCTCCGACGAGGTGCTCGCCGACCTGGATGAGCGGGGCATCGTGCGCATCGGTGCCGAGGTTCGCGACGGGGACATCCTGGTCGGCAAGGTCACCCCGAAGGGTGAGACCGAGCTGACGCCGGAGGAGCGGCTGCTGCGTGCCATCTTCGGTGAGAAGGCCCGCGAGGTGCGCGACACTTCGCTGAAGGTGCCGCACGGCGAATCCGGCAAGGTGATCGGCATTCGGGTGTTTTCCCGCGAGGACGAGGACGAGTTGCCGGCCGGTGTCAACGAGCTGGTGCGTGTGTATGTGGCTCAGAAACGCAAGATCTCCGACGGTGACAAGCTGGCCGGCCGGCACGGCAACAAGGGCGTGATCGGCAAGATCCTGCCGGTTGAGGACATGCCGTTCCTTGCCGACGGCACCCCGGTGGACATTATTTTGAACACCCACGGCGTGCCGCGACGGATGAACATCGGCCAGATTTTGGAGACCCACCTGGGTTGGTGTGCCCACAGCGGCTGGAAGGTCGACGCCGCCAAGGGGGTTCCGGACTGGGCCGCCAGGCTGCCCGACGAACTGCTCGAGGCGCAGCCGAACGCCATTGTGTCGACGCCGGTGTTCGACGGCGCCCAGGAGGCCGAGCTGCAGGGCCTGTTGTCGTGCACGCTGCCCAACCGCGACGGTGACGTGCTGGTCGACGCCGACGGCAAGGCCATGCTCTTCGACGGGCGCAGCGGCGAGCCGTTCCCGTACCCGGTCACGGTTGGCTACATGTACATCATGAAGCTGCACCACCTGGTGGACGACAAGATCCACGCCCGCTCCACCGGGCCGTACTCGATGATCACCCAGCAGCCGCTGGGCGGTAAGGCGCAGTTCGGTGGCCAGCGGTTCGGGGAGATGGAGTGCTGGGCCATGCAGGCCTACGGTGCTGCCTACACCCTGCAGGAGCTGTTGACCATCAAGTCCGATGACACCGTCGGCCGCGTCAAGGTGTACGAGGCGATCGTCAAGGGTGAGAACATCCCGGAGCCGGGCATCCCCGAGTCGTTCAAGGTGCTGCTCAAAGAACTGCAGTCGCTGTGCCTCAACGTCGAGGTGCTATCGAGTGACGGTGCGGCGATCGAACTGCGCGAAGGTGAGGACGAGGACCTGGAGCGGGCCGCGGCCAACCTGGGAATCAATCTGTCCCGCAACGAATCCGCAAGTGTCGAGGATCTTGCG"
+rpoB <- CodonSequence(string=string)
+#substr(rpoB,1348,1350) #Check the S450 
+
+attachProcess(rpoB, p.flanking, 1:449) #left hand side
+attachProcess(rpoB, p.flanking, 451:1172) #right hand side
+
+#Construct a deletion process proposing deletions with rate 0.00083333 according to a discrete length distribution
+d <- DiscreteDeletor(rate = 0.00083333, sizes = c(1, 2, 3, 4, 5, 6, 7), probs = c(0.2, 0.2, 0.2, 
+                                                                                  0.1, 0.1, 0.1, 0.1))
+
+#Construct an insertion process proposing insertions with rate 0.25 according to a discrete length distribution
+i <- DiscreteInsertor(rate = 0.00083333, sizes = c(1, 2, 3, 4, 5, 6, 7), probs = c(0.2, 0.2, 0.2, 
+                                                                                   0.1, 0.1, 0.1, 0.1))
+
+#Set the templete sequence for the insertion process (what to insert)
+i$templateSeq <- NucleotideSequence(length = 7, processes = list(list(p.flanking)))
+
+#Attaching the indel processes to the flanking regions.
+attachProcess(rpoB,d,1:449)
+attachProcess(rpoB,d,451:1172)
+attachProcess(rpoB,i,1:449)
+attachProcess(rpoB,i,451:1172)
+# Sample omegas from a discrete model. Omegas are dN/dS. 1 is neutral. >1 if favored, <1 if deleterious
+# Omegas are only useful to measure a general selection process on codon, not specific selection on specific codon. I have just set the omegas to 1, although this could be changed.
+omegaVarM0(rpoB, p.flanking, omega = 1, index = c(1:449, 
+                                                  451:1172))
+setRateMultipliers(rpoB, p.flanking, value = .2, index = c(1:449, 451:1172))
+# from Kumar&Jena: The single amino acid mutation at codon 450 (i.e., Ser to Leu [TCG to TTG]) of the rpoB gene is reported to be the most widespread mutation 
+# setting up the code frequencies this way means that transitions from TCG (or any other codon) to TTG is high. Since the ancestral codon is TCG, this sets the rate of loss of this codon.
+codon.freqs.S450 <- array(1,dim=c(1,61))# no stop codon
+codon.freqs.S450[4] <- 10 #TTG, twenty times higher, selected
+codon.freqs.S450 <- codon.freqs.S450/sum(codon.freqs.S450)
+p.S450 <- GY94(codon.freqs = codon.freqs.S450)
+p.S450$kappa = 2
+attachProcess(rpoB, p.S450, 450) #susceptive spot
+omegaVarM0(rpoB, p.S450, omega = 1, index = 450)
+
+
+
+
+#setRateMultipliers(rpoB, p.S450, value = .2, index = 450)
+
+#Parameter setting
+repn=1 # name of the simulation
+sp = 20 # number of species
+depth = 0.1 # depth of the tree
+k = 9
+complexity = 2 # patterns with low complexity won't be scored since they won't score very high any ways
+
+##################################
+#########Inside the loop##########
+##################################
+rep = 0
+while (rep < repn) {
+  prefix = paste0("phylosim_","sp",sp,"d0",strsplit(as.character(depth),split = '.',fixed = TRUE)[[1]][2],'_',rep)
+  # Input tree
+  #Generate a Random tree 
+  random_tree <- rtree(sp,rooted = TRUE)
+  #turn into ultrametric
+  intree <- chronos(random_tree, lambda = 1, model = "correlated")
+  #Adjust the depth
+  intree$edge.length <- intree$edge.length*depth
+  write.tree(intree,file=paste0(prefix,".tre"))
+  p <- Ntip(intree) #number of tips/species 
+  
+  
+  # One simulation
+  system.time(sim <- Simulate(PhyloSim(root.seq = rpoB, phylo = intree), quiet = T))
+  alignment.names <- names(sim$alignment[1, ])
+  # This identifies the location (index) of 450
+  index <- (1:1172)[alignment.names == 450 & !is.na(alignment.names)]
+  #plot(sim, aln.xlim=index + c(-13.5, 13.5), num.pages=1)
+  
+  # This makes a data.frame for 450 containing codons and whether they code for serine 
+  w <- data.frame(tip = intree$tip.label, codon = NA, serine = T)
+  selected.codons <- sim$alignment[,index]
+  for (kk in intree$tip.label) {
+    w$codon[w$tip == kk] <- selected.codons[names(selected.codons) == kk]
+    w$serine[w$tip == kk] <- is.element(selected.codons[names(selected.codons) == kk], serine.codons)
+  }
+  
+  # Function for simulating and saving alignments. It also saves as a separate file 
+  #the phenotype (serine or not at 450) and the codons at 450. Only simulations with
+  #at least 3 serines and 3 non-serines are saved. Serine is the original state and 
+  # non-serine is resistance state.
+  show(sum(w$serine))
+  if((sum(w$serine) >= complexity) & (sum(w$serine) <= length(w$serine)-complexity)){
+    saveAlignment(sim, file = paste0(prefix,".fas"), skip.internal = T) 
+    saveRDS(sim, file=paste0(prefix,".rds"))
+    write.csv(w, file = paste0(prefix,"_trait.csv", sep = ""))
+    rep <- rep + 1
+  } else {
+    print("This simulation ended up with two many or too few seriens, abort")
+    next
+  }
+  #Run aaf_phylosim.py and kmer_pattern.py
+  #TBSimulation Huan$ aaf_phylosim.py -k 9 -o phylokmer.dat -i rpoB_GY84_102.fas -W
+  
+  phylokmer.call <- paste0("python /usr/local/bin/aaf_phylosim.py -k ",as.character(k)," -o phylokmer.dat -i ",prefix,
+                           ".fas -W")
+  system(phylokmer.call, ignore.stdout = F)
+  #TBSimulation Huan$ python kmer_pattern.py -i rpoB_GY84_102/phylokmer.dat -o rpoB_GY84_102_
+  kmerpatter.call <- paste0("python /usr/local/bin/kmer_pattern.py -i ", prefix, 
+                            "/phylokmer.dat -o ", prefix,"_")
+  system(kmerpatter.call, ignore.stdout = F)
+  #Load in the shared kmer table
+  phylokmer <- read.delim(paste0(prefix, "/phylokmer.dat"), header=FALSE)
+  colnames(phylokmer)<-c("kmer",sort(intree$tip.label))
+  #Grep the patterns for kmers invloving S450 (kmer_list)
+  # In ropBSimulations_7Feb17.R, everything below was commented out.
+  #generate kmers including S450 (we do need to run aaf_phylosim.py first)
+  kmer<-sim$alignment[,(index-k/3+1):(index+k/3-1)]
+  kmer_list<-NULL #This does not consider possible deletions ('NA' in some codon)
+  for (x in 1:p) {
+    kmers<-paste(kmer[x,],collapse='')
+    for (j in 1:(nchar(kmers)-k+1)){
+      kmer_list<-c(kmer_list, substr(kmers, j, j+k-1))
+    } 
+  }
+  kmer_df<-data.frame(kmer=kmer_list)
+  kmer_df$position<-rep(c((nchar(kmers)-k+1):1),p)
+  #one kmer won't have different positions, too short
+  kmer_df<-unique(kmer_df)
+  #add their reverse compliment conterparts in (kmer_count only uses whoever that is alphabetically first, the original kmer or it's rc.)
+  #rc<-NULL
+  for (i in 1:nrow(kmer_df)) {
+    kmer_df<-rbind(kmer_df,data.frame(kmer=as.character(reverseComplement(DNAString(kmer_df$kmer[i]))), 
+                                      position=-kmer_df$position[i]))
+  }
+  kmer_df<-unique(kmer_df)
+  kmer_list_rc<-kmer_df$kmer
+  S450_kmers<-phylokmer[phylokmer$kmer %in% kmer_list_rc,]
+  write.csv(S450_kmers, file = paste0(prefix,"_S450.kmer"),row.names = F)
+}
+#Note that less than half of the kmers in kmer_list_rc ends up in S450_kmers because
+#1. only the original OR the rc kmer is in phylokmer
+#2. only kmers shared at least by two species are in phylokmer
+
+# Notes on S450_*.kmer vs. *_S450.kmer
+  # However the S450_kmer files I found are called: S450_*.kmer.
+  # SimulationScoring_Tony_15Nov16.R is using *_S450.kmer while SimulationScoring_4_Huan_8Feb17.R and extraS450kmer.R are using S450_*.kmer. Therefore S450_*.kmer is the most recent version. But I cannot find the rpoBSimulation.R that generates S450_*.kmer. Anyways, we will be generating and using *_S450.kmer from now on. Anything relating to S450_*.kmer is obsolete.
+  
+ 
